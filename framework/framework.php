f16e24d606c0081941c4a1011f126050fd639543
Aristeides Stathopoulos <aristath@gmail.com>
2014-04-29 16:01:44 +0300

    error reporting when timber is not installed

--- framework/framework.php
+++ framework/framework.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( ! class_exists( 'TimberCore' ) ) {
+	exit('Please install the <a href="http://wordpress.org/plugins/timber-library/">Timber</a> plugin.');
+}
+
 if ( ! defined( 'SS_FRAMEWORK' ) ) {
 	define( 'SS_FRAMEWORK', 'bootstrap' );
 }
