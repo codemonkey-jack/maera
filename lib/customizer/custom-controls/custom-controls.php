8548bb3cd4ab8757a78949d0bcacde0343fe1216
Aristeides Stathopoulos <aristath@gmail.com>
2014-05-06 02:15:01 +0300

    Checkbox

--- lib/customizer/custom-controls/custom-controls.php
+++ lib/customizer/custom-controls/custom-controls.php
@@ -111,4 +111,21 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 			<?php endforeach;
 		}
 	}
+
+	class Shoestrap_Customize_Checkbox_Control extends WP_Customize_Control {
+
+		public $type = 'text';
+
+		public function render_content() { ?>
+			<label class="customizer-checkbox">
+				<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
+				<strong><?php echo esc_html( $this->label ); ?></strong>
+
+				<?php if ( $this->description != '' ) { ?>
+					<a href="#" class="button tooltip" title="<?php echo strip_tags( esc_html( $this->description ) ); ?>">?</a>
+				<?php } ?>
+			</label>
+			<?php
+		}
+	}
 }
