cec335c877cf5703007e5e0e8b4580ed3bfe2f83
Aristeides Stathopoulos <aristath@gmail.com>
2014-05-06 05:10:50 +0300

    textarea

---
+++ lib/customizer/custom-controls/textarea.php
@@ -0,0 +1,19 @@
+<?php
+
+class SS_Customize_Textarea_Control extends WP_Customize_Control {
+
+	public $type = 'textarea';
+
+	public function render_content() { ?>
+		<label class="customizer-textarea">
+			<span class="customize-control-title">
+				<?php echo esc_html( $this->label ); ?>
+				<?php if ( isset( $this->description && '' != $this->description ) { ?>
+					<a href="#" class="button tooltip" title="<?php echo strip_tags( esc_html( $this->description ) ); ?>">?</a>
+				<?php } ?>
+			</span>
+			<textarea class="of-input" rows="5" style="width:100%;" <?php $this->link(); ?>><?php echo esc_textarea( $this->value() ); ?></textarea>
+		</label>
+		<?php
+	}
+}
