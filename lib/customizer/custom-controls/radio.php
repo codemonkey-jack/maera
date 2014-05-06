342edab0e0f826d63f2d9f822a4561e9544a0642
Aristeides Stathopoulos <aristath@gmail.com>
2014-05-06 05:06:41 +0300

    radio

---
+++ lib/customizer/custom-controls/radio.php
@@ -0,0 +1,32 @@
+<?php
+
+class SS_Customize_Radio_Control extends WP_Customize_Control {
+
+	public $type = 'radio';
+
+	public function render_content() {
+
+		if ( empty( $this->choices ) ) {
+			return;
+		}
+
+		$name = '_customize-radio-' . $this->id;
+
+		?>
+		<span class="customize-control-title">
+			<?php echo esc_html( $this->label ); ?>
+			<?php if ( isset( $this->description ) && '' != $this->description ) { ?>
+				<a href="#" class="button tooltip" title="<?php echo strip_tags( esc_html( $this->description ) ); ?>">?</a>
+			<?php } ?>
+		</span>
+		<?php
+		foreach ( $this->choices as $value => $label ) :
+			?>
+			<label class="customizer-radio">
+				<input type="radio" value="<?php echo esc_attr( $value ); ?>" name="<?php echo esc_attr( $name ); ?>" <?php $this->link(); checked( $this->value(), $value ); ?> />
+				<?php echo esc_html( $label ); ?><br/>
+			</label>
+			<?php
+		endforeach;
+	}
+}
