763538b889392b1c0071ee64edffe1100473f0a3
Aristeides Stathopoulos <aristath@gmail.com>
2014-05-06 13:02:10 +0300

    text control

---
+++ lib/customizer/custom-controls/text.php
@@ -0,0 +1,18 @@
+<?php
+
+class SS_Customize_Text_Control extends WP_Customize_Control {
+
+	public $type = 'text';
+
+	public function render_content() { ?>
+
+		<label class="customizer-text">
+			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<input type="text" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); ?> />
+			<?php if ( isset( $this->description ) && '' != $this->description ) { ?>
+				<a href="#" class="button tooltip" title="<?php echo strip_tags( esc_html( $this->description ) ); ?>">?</a>
+			<?php } ?>
+		</label>
+		<?php
+	}
+}
