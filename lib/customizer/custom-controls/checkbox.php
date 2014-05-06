4703107be719e27690d63d660e97582574402221
Aristeides Stathopoulos <aristath@gmail.com>
2014-05-06 04:58:25 +0300

    checkbox

---
+++ lib/customizer/custom-controls/checkbox.php
@@ -0,0 +1,17 @@
+<?php
+
+class SS_Customize_Checkbox_Control extends WP_Customize_Control {
+
+	public $type = 'checkbox';
+
+	public function render_content() { ?>
+		<label class="customizer-checkbox">
+			<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
+			<strong><?php echo esc_html( $this->label ); ?></strong>
+			<?php if ( isset( $this->description ) && '' != $this->description ) { ?>
+				<a href="#" class="button tooltip" title="<?php echo strip_tags( esc_html( $this->description ) ); ?>">?</a>
+			<?php } ?>
+		</label>
+		<?php
+	}
+}
