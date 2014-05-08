15b906dd86f6c58a4553153a54e762428cf1d26b
Aristeides Stathopoulos <aristath@gmail.com>
2014-05-08 14:52:46 +0300

    Addes menu styling options

--- framework/bootstrap/customizer/menus.php
+++ framework/bootstrap/customizer/menus.php
@@ -40,7 +40,7 @@ function shoestrap_menus_customizer_settings( $controls ){
 
 	$controls[] = array(
 		'type'     => 'checkbox',
-		'setting'  => 'jumbotron_center',
+		'setting'  => 'navbar_logo',
 		'label'    => __( 'Use Logo ( if available ) for branding.', 'shoestrap' ),
 		'description' => __( 'If this option is not checked, or there is no logo available, then the sitename will be displayed instead.', 'shoestrap' ),
 		'section'  => 'menus',
@@ -121,6 +121,150 @@ function shoestrap_menus_customizer_settings( $controls ){
 		),
 		'priority' => 7,
 	);
+
+	$controls[] = array(
+		'type'     => 'color',
+		'setting'  => 'navbar_bg',
+		'label'    => __( 'NavBar Background Color', 'shoestrap' ),
+		'description' => __( 'Pick a background color for the NavBar. Default: #f8f8f8.', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => '#f8f8f8',
+		'priority' => 10,
+	);
+
+	$controls[] = array(
+		'type'     => 'slider',
+		'setting'  => 'navbar_bg_opacity',
+		'label'    => __( 'NavBar Background Opacity', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => 100,
+		'priority' => 11,
+		'choices'  => array(
+			'min'  => 0,
+			'max'  => 100,
+			'step' => 1,
+		),
+	);
+
+	$controls[] = array(
+		'type'     => 'select',
+		'setting'  => 'navbar_style',
+		'label'    => __( 'Navbar Style', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => 'default',
+		'choices'  => array(
+			'default' => __( 'Default', 'shoestrap' ),
+			'style1'  => __( 'Style', 'shoestrap' ) . ' 1',
+			'style2'  => __( 'Style', 'shoestrap' ) . ' 2',
+			'style3'  => __( 'Style', 'shoestrap' ) . ' 3',
+			'style4'  => __( 'Style', 'shoestrap' ) . ' 4',
+			'style5'  => __( 'Style', 'shoestrap' ) . ' 5',
+			'style6'  => __( 'Style', 'shoestrap' ) . ' 6',
+			'metro'   => __( 'Metro', 'shoestrap' ),
+		),
+		'priority' => 12,
+	);
+
+	$controls[] = array(
+		'type'     => 'slider',
+		'setting'  => 'navbar_height',
+		'label'    => __( 'NavBar Height', 'shoestrap' ),
+		'subtitle' => __( 'Select the height of your navbars in pixels.', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => 50,
+		'priority' => 13,
+		'choices'  => array(
+			'min'  => 38,
+			'max'  => 200,
+			'step' => 1,
+		),
+	);
+
+	$controls[] = array(
+		'type'     => 'text',
+		'setting'  => 'font_menus_font_family',
+		'label'    => __( 'Menus font', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => '"Helvetica Neue", Helvetica, Arial, sans-serif',
+		'priority' => 20,
+	);
+
+	$controls[] = array(
+		'type'     => 'checkbox',
+		'setting'  => 'font_menus_google',
+		'label'    => __( 'Google-Font', 'shoestrap' ),
+		'description' => __( 'If you have entered the name of a google font above, then you must enable check this option to process it.', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => 0,
+		'priority' => 21,
+	);
+
+	$controls[] = array(
+		'type'     => 'color',
+		'setting'  => 'font_menus_color',
+		'description' => __( 'Font Color', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => '#333333',
+		'priority' => 22,
+	);
+
+	$controls[] = array(
+		'type'     => 'slider',
+		'setting'  => 'font_menus_weight',
+		'subtitle' => __( 'Font Weight', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => 400,
+		'priority' => 23,
+		'choices'  => array(
+			'min'  => 100,
+			'max'  => 800,
+			'step' => 100,
+		),
+	);
+
+	$controls[] = array(
+		'type'     => 'slider',
+		'setting'  => 'font_menus_size',
+		'subtitle' => __( 'Font Size (px)', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => 20,
+		'priority' => 24,
+		'choices'  => array(
+			'min'  => 7,
+			'max'  => 70,
+			'step' => 1,
+		),
+	);
+
+	$controls[] = array(
+		'type'     => 'slider',
+		'setting'  => 'font_menus_height',
+		'subtitle' => __( 'Line Height (px)', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => 22,
+		'priority' => 25,
+		'choices'  => array(
+			'min'  => 7,
+			'max'  => 70,
+			'step' => 1,
+		),
+		'separator' => true
+	);
+
+	$controls[] = array(
+		'type'     => 'slider',
+		'setting'  => 'navbar_margin',
+		'subtitle' => __( 'Select the top and bottom margin of the NavBar in pixels. Applies only in static top navbars. Default: 0px.', 'shoestrap' ),
+		'section'  => 'menus',
+		'default'  => 0,
+		'priority' => 30,
+		'choices'  => array(
+			'min'  => 0,
+			'max'  => 200,
+			'step' => 1,
+		),
+	);
+
 
 	return $controls;
 }
