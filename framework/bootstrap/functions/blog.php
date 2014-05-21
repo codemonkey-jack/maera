9f0ec81b53321b499a118aaacb6e9eca02f76471
Aristeides Stathopoulos <aristath@gmail.com>
2014-05-21 22:23:07 +0300

    The "more" text filter now works

--- framework/bootstrap/functions/blog.php
+++ framework/bootstrap/functions/blog.php
@@ -15,7 +15,23 @@ function shoestrap_breadcrumbs() {
 add_action( 'shoestrap/content/before', 'shoestrap_breadcrumbs' );
 
 
+/**
+ * Excerpt length
+ */
 function shoestrap_excerpt_length() {
+
 	return get_theme_mod( 'post_excerpt_length', 55 );
+
 }
 add_filter( 'excerpt_length', 'shoestrap_excerpt_length' );
+
+/**
+ * The "more" text
+ */
+function shoestrap_excerpt_more( $more ) {
+
+	$continue_text = get_theme_mod( 'post_excerpt_link_text', 'Continued' );
+	return ' &hellip; <a href="' . get_permalink() . '">' . $continue_text . '</a>';
+
+}
+add_filter( 'excerpt_more', 'shoestrap_excerpt_more' );
