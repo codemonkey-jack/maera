e5e457e882d5e6239f7d0fda137e44b14b46a734
Aristeides Stathopoulos <aristath@gmail.com>
2014-09-08 14:58:36 +0300

    improve wp_title

--- lib/init.php
+++ lib/init.php
@@ -112,3 +112,42 @@ function maera_add_to_twig( $twig ){
 	return $twig;
 }
 add_filter( 'get_twig', 'maera_add_to_twig' );
+
+
+/**
+ * Create a nicely formatted and more specific title element text for output
+ * in head of document, based on current view.
+ *
+ * @since Twenty Fourteen 1.0
+ *
+ * @global int $paged WordPress archive pagination page count.
+ * @global int $page  WordPress paginated post page count.
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string The filtered title.
+ */
+function maera_wp_title( $title, $sep = '|' ) {
+	global $paged, $page;
+
+	if ( is_feed() ) {
+		return $title;
+	}
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name', 'display' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title = "$title $sep $site_description";
+	}
+
+	// Add a page number if necessary.
+	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'maera' ), max( $paged, $page ) );
+	}
+
+	return $title;
+}
+add_filter( 'wp_title', 'maera_wp_title', 10, 2 );
