59f9f584a4f5750f57b354101c84eb06adfcad57
fovoc <kalliris.d@gmail.com>
2014-09-20 15:44:09 +0300

    minor bugfix

--- framework/core/assets/js/vendor/menu.js
+++ framework/core/assets/js/vendor/menu.js
@@ -1,13 +1,7 @@
 (function($) {
 	$(function() {
-		if ( $( "body" ).hasClass( "show-menu" ) ) {
-			$( ".menu-button" ).click( function() {
-				$( "body" ).removeClass( "show-menu" );
-			});
-		} else {
-			$( ".menu-button" ).click( function() {
-				$( "body" ).addClass( "show-menu" );
-			});
-		}
+		$( ".menu-button" ).click( function() {
+			$( "body" ).toggleClass( "show-menu" );
+		});
 	});
 })(jQuery);
