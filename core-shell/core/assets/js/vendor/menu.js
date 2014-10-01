4c7dfc4b756212ae624e395097666244968afed8
fovoc <kalliris.d@gmail.com>
2014-10-01 22:23:59 +0300

    hide menu if click outside the left sidebar

--- core-shell/core/assets/js/vendor/menu.js
+++ core-shell/core/assets/js/vendor/menu.js
@@ -3,5 +3,8 @@
 		$( ".menu-button" ).click( function() {
 			$( "body" ).toggleClass( "show-menu" );
 		});
+		$( "#layout, .page-header" ).click( function() {
+			$( "body" ).removeClass( "show-menu" );
+		});
 	});
 })(jQuery);
