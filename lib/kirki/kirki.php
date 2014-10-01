79da3b5226b2e10995ff8ffde58598577b90a681
Aristeides Stathopoulos <aristath@gmail.com>
2014-10-01 22:55:16 +0300

    Updating Kirki

--- lib/kirki/kirki.php
+++ lib/kirki/kirki.php
@@ -30,9 +30,6 @@ class Kirki {
 		// Include the controls initialization script
 		include_once( dirname( __FILE__ ) . '/includes/controls/controls-init.php' );
 
-		// Include the fonts initialization scripts.
-		include_once( dirname( __FILE__ ) . '/includes/functions/class-Kirki_Fonts.php' );
-
 		add_action( 'customize_register', array( $this, 'include_files' ), 1 );
 		add_action( 'customize_controls_print_styles', array( $this, 'styles' ) );
 		add_action( 'customize_controls_print_styles', array( $this, 'googlefonts' ) );
@@ -91,48 +88,159 @@ class Kirki {
 	 */
 	function custom_css() {
 
+		$admin_theme = get_user_meta( get_current_user_id(), 'admin_color', true ); //Find out which theme the user has selected.
+
 		$options = apply_filters( 'kirki/config', array() );
 
-		$color_active = isset( $options['color_active'] ) ? $options['color_active'] : '#1abc9c';
-		$color_accent = isset( $options['color_accent'] ) ? $options['color_accent'] : '#FF5740';
-		$color_light  = isset( $options['color_light'] ) ? $options['color_light'] : '#8cddcd';
-		$color_select = isset( $options['color_select'] ) ? $options['color_select'] : '#34495e';
-		$color_back = isset( $options['color_back'] ) ? $options['color_back'] : '#222222';
+		switch ( $admin_theme ){
+			// Light theme
+			case 'light':
+				$color_font = '#999';
+				$color_active = '#999';
+				$color_light = '#04a4cc';
+				$color_select = '#f1f1f1';
+				$color_accent = '#f1f1f1';
+				$color_content = '#fff';
+				$color_back = '#e5e5e5';
+			break;
+			// Blue theme
+			case 'blue':
+				$color_font = '#fff';
+				$color_active = '#096484';
+				$color_light = '#096484';
+				$color_select = '#fff';
+				$color_accent = '#fff';
+				$color_content = '#f1f1f1';
+				$color_back = '#74B6CE';
+			break;
+			// Coffee theme
+			case 'coffee':
+				$color_font = '#fff';
+				$color_active = '#c7a589';
+				$color_light = '#c7a589';
+				$color_select = '#fff';
+				$color_accent = '#fff';
+				$color_content = '#f1f1f1';
+				$color_back = '#59524c';
+			break;
+			// Ectoplasm theme
+			case 'ectoplasm':
+				$color_font = '#fff';
+				$color_active = '#a3b745';
+				$color_light = '#a3b745';
+				$color_select = '#fff';
+				$color_accent = '#fff';
+				$color_content = '#f1f1f1';
+				$color_back = '#523f6d';
+			break;
+			// Midnight theme
+			case 'midnight':
+				$color_font = '#fff';
+				$color_active = '#e14d43';
+				$color_light = '#e14d43';
+				$color_select = '#fff';
+				$color_accent = '#fff';
+				$color_content = '#f1f1f1';
+				$color_back = '#363b3f';
+			break;
+			// Ocean theme
+			case 'ocean':
+				$color_font = '#fff';
+				$color_active = '#9ebaa0';
+				$color_light = '#9ebaa0';
+				$color_select = '#fff';
+				$color_accent = '#fff';
+				$color_content = '#f1f1f1';
+				$color_back = '#738e96';
+			break;
+			// Sunrise theme
+			case 'sunrise':
+				$color_font = '#fff';
+				$color_active = '#dd823b';
+				$color_light = '#dd823b';
+				$color_select = '#fff';
+				$color_accent = '#fff';
+				$color_content = '#f1f1f1';
+				$color_back = '#cf4944';
+			break;
+			// Default WordPress theme "Fresh"
+			default:
+				$color_font = isset( $options['color_font'] ) ? $options['color_font'] : '#fff';
+				$color_active = isset( $options['color_active'] ) ? $options['color_active'] : '#2ea2cc';
+				$color_light  = isset( $options['color_light'] ) ? $options['color_light'] : '#2ea2cc';
+				$color_select = isset( $options['color_select'] ) ? $options['color_select'] : '#fff';
+				$color_accent = isset( $options['color_accent'] ) ? $options['color_accent'] : '#fff';
+				$color_content = isset( $options['color_content'] ) ? $options['color_content'] : '#f1f1f1';
+				$color_back = isset( $options['color_back'] ) ? $options['color_back'] : '#222';
+		}
+
 		?>
 
 		<style>
+			.wp-core-ui .button.tooltip {
+				background: <?php echo $color_active; ?>;
+			}
 
-			<?php if ( 170 < kirki_get_brightness( $color_back ) ) : ?>
-				#customize-theme-controls .accordion-section-title { background: <?php echo $color_back; ?>; color: #222; border-bottom: 1px solid rgba(255,255,255,0.05); }
-				.control-section.control-panel>.accordion-section-title:after { background: rgba(255,255,255,0.15); border-left: rgba(255,255,255,0.2); color: #222; }
-				#customize-theme-controls .control-section .accordion-section-title, #customize-theme-controls .control-section.control-panel .accordion-section-title { color: #222; }
-				#customize-theme-controls .control-section .accordion-section-title:focus, #customize-theme-controls .control-section .accordion-section-title:hover, #customize-theme-controls .control-section.open .accordion-section-title, #customize-theme-controls .control-section:hover .accordion-section-title { background: <?php echo $color_active; ?>; color: #222; }
-			<?php else : ?>
-				#customize-theme-controls .accordion-section-title { background: <?php echo $color_back; ?>; color: #fff; border-bottom: 1px solid rgba(0,0,0,0.05);}
-				.control-section.control-panel>.accordion-section-title:after { background: rgba(0,0,0,0.15); border-left: rgba(0,0,0,0.2); color: #fff; }
-				#customize-theme-controls .control-section .accordion-section-title, #customize-theme-controls .control-section.control-panel .accordion-section-title { color: #fff; }
-				#customize-theme-controls .control-section .accordion-section-title:focus, #customize-theme-controls .control-section .accordion-section-title:hover, #customize-theme-controls .control-section.open .accordion-section-title, #customize-theme-controls .control-section:hover .accordion-section-title { background: <?php echo $color_active; ?>; color: #fff; }
-			<?php endif; ?>
+			.image.ui-buttonset label.ui-button.ui-state-active {
+				background: <?php echo $color_accent; ?>;
+			}
 
-			<?php if ( kirki_get_brightness( $color_accent ) > 100 ) : ?>
-				#customize-theme-controls .control-section.control-panel>h3.accordion-section-title:focus:after, #customize-theme-controls .control-section.control-panel>h3.accordion-section-title:hover:after { background: rgba(255,255,255,0.3); border: none; color: #222; }
-			<?php else : ?>
-				#customize-theme-controls .control-section.control-panel>h3.accordion-section-title:focus:after, #customize-theme-controls .control-section.control-panel>h3.accordion-section-title:hover:after { background: rgba(0,0,0,0.3); border: none; color: #fff; }
-			<?php endif; ?>
+			.wp-full-overlay-sidebar {
+				background: <?php echo $color_back; ?>;
+			}
 
-			.wp-core-ui .button.tooltip { background: <?php echo $color_active; ?>; }
-			.image.ui-buttonset label.ui-button.ui-state-active { background: <?php echo $color_accent; ?>; }
-			.wp-full-overlay-sidebar { background: <?php echo $color_back; ?>; }
-			#customize-info .accordion-section-title, #customize-info .accordion-section-title:hover { background: <?php echo $color_back; ?>; }
+			#customize-info .accordion-section-title, #customize-info .accordion-section-title:hover {
+				background: <?php echo $color_back; ?>;
+			}
 
-			#customize-theme-controls .control-section.control-panel .accordion-section-title { background: <?php echo $color_back; ?>; }
-			#customize-theme-controls .control-section.control-panel .accordion-section-title:focus, #customize-theme-controls .control-section.control-panel .accordion-section-title:hover { background: <?php echo $color_active; ?>; }
-			.wp-core-ui .button-primary { background: <?php echo $color_active; ?>; }
-			.wp-core-ui .button-primary.focus, .wp-core-ui .button-primary.hover, .wp-core-ui .button-primary:focus, .wp-core-ui .button-primary:hover { background: <?php echo $color_select; ?>; }
-			.wp-core-ui .button-primary-disabled, .wp-core-ui .button-primary.disabled, .wp-core-ui .button-primary:disabled, .wp-core-ui .button-primary[disabled] { background: <?php echo $color_light; ?> !important; color: <?php echo $color_select; ?> !important; }
+			#customize-theme-controls .accordion-section-title {
+				background: <?php echo $color_back; ?>;
+			}
+
+			#customize-theme-controls .accordion-section-title {
+				border-bottom: 1px solid <?php echo $color_back; ?>;
+			}
+
+			#customize-theme-controls .control-section .accordion-section-title {
+				color: <?php echo $color_font; ?>;
+				background: <?php echo $color_back; ?>;
+			}
+
+			#customize-theme-controls .control-section .accordion-section-title:focus,
+			#customize-theme-controls .control-section .accordion-section-title:hover,
+			#customize-theme-controls .control-section.open .accordion-section-title,
+			#customize-theme-controls .control-section:hover .accordion-section-title {
+				color: <?php echo $color_content; ?>;
+				background: <?php echo $color_active; ?>;
+			}
+
+			#customize-theme-controls .accordion-section-content{
+				background: <?php echo $color_content; ?>;
+			}
+
+			.wp-core-ui .button-primary {
+				background: <?php echo $color_active; ?>;
+			}
+
+			.wp-core-ui .button-primary.focus,
+			.wp-core-ui .button-primary.hover,
+			.wp-core-ui .button-primary:focus,
+			.wp-core-ui .button-primary:hover {
+				background: <?php echo $color_select; ?>;
+			}
+
+			.wp-core-ui .button-primary-disabled,
+			.wp-core-ui .button-primary.disabled,
+			.wp-core-ui .button-primary:disabled,
+			.wp-core-ui .button-primary[disabled] {
+				background: <?php echo $color_light; ?> !important;
+				color: <?php echo $color_select; ?> !important;
+			}
 
 			<?php if ( isset( $options['logo_image'] ) ) : ?>
-				div.kirki-customizer { background: url("<?php echo $options['logo_image']; ?>") no-repeat left center; }
+				div.kirki-customizer {
+					background: url("<?php echo $options['logo_image']; ?>") no-repeat left center;
+				}
 			<?php endif; ?>
 		</style>
 		<?php
