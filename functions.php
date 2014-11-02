14e8deea3d080ca288a4d605b6d9fcc1396902e7
Aristeides Stathopoulos <aristath@gmail.com>
2014-11-02 05:16:40 +0200

    update modernizr + scripts loading paths

--- functions.php
+++ functions.php
@@ -119,11 +119,11 @@ class Maera {
 		wp_enqueue_script( 'maera-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 		// Enqueue Modernizr
-		wp_register_script( 'modernizr', MAERA_ASSETS_URL . '/js/modernizr.min.js', false, null, false );
+		wp_register_script( 'modernizr', get_template_directory_uri() . '/assets/js/modernizr.min.js', false, null, false );
 		wp_enqueue_script( 'modernizr' );
 
 		// Enqueue fitvids
-		wp_register_script( 'fitvids', MAERA_ASSETS_URL . '/js/jquery.fitvids.js',false, null, true  );
+		wp_register_script( 'fitvids', get_template_directory_uri() . '/assets/js/jquery.fitvids.js',false, null, true  );
 		wp_enqueue_script( 'fitvids' );
 
 		// Enqueue jQuery
