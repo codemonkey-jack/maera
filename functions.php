991c88ce3827bbfcba154b44bcb8092e977b3081
Aristeides Stathopoulos <aristath@gmail.com>
2014-11-12 15:16:19 +0200

    BP works

--- functions.php
+++ functions.php
@@ -29,10 +29,6 @@ class Maera {
 		$maera_shell = new Maera_Shell();
 		$maera_init  = new Maera_Init();
 
-		if ( class_exists( 'BuddyPress' ) ) {
-			add_action( 'bp_include', array( $this, 'bp_include' ) );
-		}
-
 	}
 
 	function requires() {
@@ -47,10 +43,6 @@ class Maera {
 		require_once locate_template( '/lib/admin-page.php' );
 		require_once locate_template( '/lib/updater/updater.php' );
 
-	}
-
-	function bp_include() {
-		require_once locate_template( '/lib/timber-bp.php' );
 	}
 
 	function content_width() {
