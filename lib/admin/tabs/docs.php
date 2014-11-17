59b5b73b3f51e1a22903bc66322798f32e938861
Aristeides Stathopoulos <aristath@gmail.com>
2014-11-17 17:10:35 +0200

    make the docs tabs a bit more modular

--- lib/admin/tabs/docs.php
+++ lib/admin/tabs/docs.php
@@ -1,48 +1,34 @@
-<!-- Begin Header -->
+<?php
+
+$docs_tabs = array(
+		'general'       => 'General',
+		'templating'    => 'Templating',
+		'shells'        => 'Shells',
+		'actions-hooks' => 'Actions & Hooks',
+);
+?>
 <div id="docs_header">
 	<h2>This tab will contain the theme documentation.</h2>
 	<p>For the time being you can see partial docs in the <a href="https://github.com/presscodes/maera/wiki">Repository's Wiki</a> on github.</p>
 </div>
-<!-- End Header -->
 
-<!-- Begin Tabs -->
-<div id="tabs-vertical">
-	<!-- Begin Tab Titles -->
+<div id="docs-tabs">
 	<ul>
-		<li><a href="#tab-1">Topic 1</a></li>
-		<li><a href="#tab-2">Topic 2</a></li>
-		<li><a href="#tab-3">Topic 3</a></li>
-		<li><a href="#tab-4">Topic 4</a></li>
-		<li><a href="#tab-5">Topic 5</a></li>
-		<li><a href="#tab-6">Topic 6</a></li>
+		<?php foreach ( $docs_tabs as $docs_tab => $docs_tab_label ) : ?>
+			<li><a href="#<?php echo $docs_tab; ?>"><?php echo $docs_tab_label; ?></a></li>
+		<?php endforeach; ?>
 	</ul>
-	<!-- End Tab Titles -->
-	<!-- Begin Tab Content -->
-	<div id="tab-1">
-		<p>Lorem</p>
-	</div>
-	<div id="tab-2">
-		<p>Ipsum</p>
-	</div>
-	<div id="tab-3">
-		<p>Vehicula</p>
-	</div>
-	<div id="tab-4">
-		<p>Lorem</p>
-	</div>
-	<div id="tab-5">
-		<p>Ipsum</p>
-	</div>
-	<div id="tab-6">
-		<p>Vehicula</p>
-	</div>
-	<!-- End Tab Content -->
+
+	<?php foreach ( $docs_tabs as $docs_tab => $docs_tab_label ) : ?>
+		<div id="<?php echo $docs_tab; ?>">
+			<h2><?php echo $docs_tab_label; ?></h2>
+			<?php include( dirname( __FILE__ ) . '/docs/' . $docs_tab . '.php' ); ?>
+		</div>
+	<?php endforeach; ?>
 </div>
-<!-- End Tabs -->
 
 <script>
 jQuery(document).ready(function($) {
-	$( "#tabs-vertical" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
-	$( "#tabs-vertical li" ).tabs().removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+	$( "#docs-tabs" ).tabs();
 });
 </script>
