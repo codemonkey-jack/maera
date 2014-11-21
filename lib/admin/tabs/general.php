2a4a6a50c585993d2f1fa0414fb73428f367c108
Aristeides Stathopoulos <aristath@gmail.com>
2014-11-21 23:31:45 +0200

    general page content

--- lib/admin/tabs/general.php
+++ lib/admin/tabs/general.php
@@ -1,4 +1,8 @@
-<h1 class="maera-general-title">Welcome to the Maera Theme</h1>
-<hr>
-<p>Maera is a WordPress Theme Framework.</p>
-<p>More info coming soon.</p>
+<div class="maera-general-content">
+	<h1 class="maera-general-title">Welcome to the Maera Theme</h1>
+	<hr>
+	<p class="big">Maera is one of the most versatile and modular WordPress Theme Frameworks, allowing both developers and novice users to build amazing sites and take advantage of its unique features.</p>
+	<div style="height: 1em;"></div>
+	<p>Maera introduces the concept of <strong>shells</strong>, essentially child themes built as plugins that can change and affect every aspect of your theme and presentation, allowing you to build a completely customized experience for your site and your users.</p>
+	<p>Combined with the <a href="http://twig.sensiolabs.org/">twig</a> templating engine, creating a custom theme is easier than ever!</p>
+</div>
