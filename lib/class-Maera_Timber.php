8e8f9347ad3bf301d101246741f41d364e5ecfe0
Aristeides Stathopoulos <aristath@gmail.com>
2014-12-19 21:22:31 +0200

    add a customizer conditional to timber caching logic

--- lib/class-Maera_Timber.php
+++ lib/class-Maera_Timber.php
@@ -146,12 +146,14 @@ class Maera_Timber extends Maera {
 	 */
 	function timber_customizations() {
 
+		global $wp_customize;
+
 		$locations = self::twig_locations();
 		Timber::$locations = $locations;
 
 		// Add caching if dev_mode is set to off.
 		$theme_options = get_option( 'maera_admin_options', array() );
-		if ( isset( $theme_options['dev_mode'] ) && 0 == $theme_options['dev_mode'] ) {
+		if ( isset( $theme_options['dev_mode'] ) && 0 == $theme_options['dev_mode'] && ! isset( $wp_customize ) ) {
 
 			add_filter( 'maera/styles/caching', '__return_true' );
 			// Turn on Timber caching.
