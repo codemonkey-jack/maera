71d744d197a0baad1d1e64163e78e99f0bd35218
Aristeides Stathopoulos <aristath@gmail.com>
2014-12-23 01:10:43 +0200

    include the plugins via git cloning them and using require.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
 define( 'WP_DEFAULT_THEME', 'maera' );
+define( 'JETPACK_DEV_DEBUG', true);
+
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
@@ -8,5 +10,9 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 // 	require dirname( __FILE__ ) . '/../functions.php';
 // }
 // tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require WP_PLUGIN_DIR . '/jetpack/jetpack.php'
+require WP_PLUGIN_DIR . '/timber/timber.php'
+require WP_PLUGIN_DIR . '/kirki/kirki.php'
 
 require $_tests_dir . '/includes/bootstrap.php';
