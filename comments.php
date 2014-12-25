6519b77d0e551f51ae2c669e51ba834856cd9a09
Aristeides Stathopoulos <aristath@gmail.com>
2014-12-25 18:26:36 +0200

    Updating content & comments

--- comments.php
+++ comments.php
@@ -8,23 +8,17 @@
  * @package maera
  */
 
-$context = Maera_Timber::get_context();
-$post = new TimberPost();
-$context['post'] = $post;
-
-// If the current post is protected by a password and
-// the visitor has not yet entered the password we will
-// return early without loading the comments.
+/**
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
 if ( post_password_required( $post ) ) {
 	return;
 }
 
-Timber::render(
-	array(
-		'comments-' . $post->ID . '.twig',
-		$post->post_type . '-comments.twig',
-		'comments.twig'
-	),
-	$context,
-	apply_filters( 'maera/timber/cache', false )
-);
+Maera_Template::content( array(
+	'comments-' . $post->ID . '.twig',
+	$post->post_type . '-comments.twig',
+	'comments.twig'
+));
