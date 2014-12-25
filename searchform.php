a86b2a481947f4fb1b0d6d83ba444cc646adc89d
Aristeides Stathopoulos <aristath@gmail.com>
2014-12-25 18:53:15 +0200

    searchform

--- searchform.php
+++ searchform.php
@@ -3,8 +3,4 @@
  * Search Form
  */
 
-Timber::render(
-	'searchform.twig',
-	Maera_Timber::get_context(),
-	apply_filters( 'maera/timber/cache', false )
-);
+Maera_Template::content( 'searchform.twig' );
