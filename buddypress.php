698cfd3633c19c13fc5386fd30ae676d484b0449
Aristeides Stathopoulos <aristath@gmail.com>
2015-01-05 20:09:19 +0200

    BuddyPress Compatibility

--- buddypress.php
+++ buddypress.php
@@ -2,6 +2,11 @@
 
 Maera_Template::dependencies();
 
+$context = Maera_Timber::get_context();
+$post = Timber::query_post();
+$context['post'] = $post;
+$context['content'] = maera_get_echo( 'the_content' );
+
 Maera_Template::header();
-Maera_Template::main( 'bp.twig' );
+Maera_Template::main( 'bp.twig', $context );
 Maera_Template::footer();
