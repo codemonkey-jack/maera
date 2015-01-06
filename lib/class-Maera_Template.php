072c01f3665c52bf13f05ec6a7d1e945ea239ce7
Aristeides Stathopoulos <aristath@gmail.com>
2015-01-06 21:14:19 +0200

    Use context_archives_title() (introduced in WP 4.1)

--- lib/class-Maera_Template.php
+++ lib/class-Maera_Template.php
@@ -79,7 +79,7 @@ class Maera_Template {
 
 		if ( is_archive() || is_home() ) {
 			$context['posts'] = Timber::query_posts( false, 'TimberPost' );
-			$context['title'] = self::context_archives_title();
+			$context['title'] = get_the_archive_title();
 
 			if ( is_author() ) {
 				$author = new TimberUser( $wp_query->query_vars['author'] );
@@ -89,51 +89,6 @@ class Maera_Template {
 		}
 
 		return $context;
-	}
-
-	/**
-	 * Determine the context that will be used by the content() method
-	 */
-	public static function context_archives_title() {
-
-		if ( is_day() ) {
-			return sprintf( __( 'Day: %s', 'maera' ), '<span>' . get_the_date() . '</span>' );
-		} elseif ( is_month() ) {
-			return sprintf( __( 'Month: %s', 'maera' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'maera' ) ) . '</span>' );
-		} elseif ( is_year() ) {
-			return sprintf( __( 'Year: %s', 'maera' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'maera' ) ) . '</span>' );
-		} elseif ( is_tag() ) {
-			return single_tag_title( '', false );
-		} elseif ( is_category() ) {
-			return single_cat_title( '', false );
-		} elseif ( is_post_type_archive() ) {
-			return post_type_archive_title( '', false );
-		} elseif ( is_author() ) {
-			return sprintf( __( 'Author: %s', 'maera' ), '<span class="vcard">' . get_the_author() . '</span>' );
-		} elseif ( is_tax( 'post_format' ) ) {
-			if ( is_tax( 'post_format', 'post-format-aside' ) ) {
-				return __( 'Asides', 'maera' );
-			} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
-				return __( 'Galleries', 'maera' );
-			} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
-				return __( 'Images', 'maera');
-			} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
-				return __( 'Videos', 'maera' );
-			} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
-				return __( 'Quotes', 'maera' );
-			} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
-				return __( 'Links', 'maera' );
-			} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
-				return __( 'Statuses', 'maera' );
-			} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
-				return __( 'Audios', 'maera' );
-			} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
-				return __( 'Chats', 'maera' );
-			}
-		} else {
-			return __( 'Archive', 'maera' );
-		}
-
 	}
 
 	/**
