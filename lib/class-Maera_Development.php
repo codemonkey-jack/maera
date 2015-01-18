ea96055986d5850427251ad60f2f22c6583a3827
Aristeides Stathopoulos <aristath@gmail.com>
2015-01-19 00:58:07 +0200

    fixes #132

--- lib/class-Maera_Development.php
+++ lib/class-Maera_Development.php
@@ -17,13 +17,19 @@ class Maera_Development {
 	public static function dev_mode() {
 
 		global $wp_customize;
+		$options = get_option( 'maera_admin_options', array() );
 
-		$theme_options = get_option( 'maera_admin_options', array() );
-		if ( isset( $theme_options['dev_mode'] ) && 0 == $theme_options['dev_mode'] && ! isset( $wp_customize ) ) {
-			return false;
-		} else {
+		// If we're on the customizer, return true.
+		if ( isset( $wp_customize ) ) {
+			return;
+		}
+
+		// If dev mode is enabled on the dashboard, return true;
+		if ( isset( $options['dev_mode'] ) && 1 == $options['dev_mode'] ) {
 			return true;
 		}
+
+		return false;
 
 	}
 
