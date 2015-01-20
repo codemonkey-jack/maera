a682ec7b3cb6c2dddd7fa9395750e95314868657
Aristeides Stathopoulos <aristath@gmail.com>
2015-01-20 20:09:39 +0200

    Update class-Maera_Logo_Widget.php

--- lib/widgets/logo/includes/class-Maera_Logo_Widget.php
+++ lib/widgets/logo/includes/class-Maera_Logo_Widget.php
@@ -44,12 +44,7 @@ class Maera_Logo_Widget extends WP_Widget {
 	 * @since 1.0.0
 	 */
 	public function widget( $args, $instance ) {
-
-		Timber::render(
-			'site-logo.twig',
-			Maera_Caching::get_context(),
-			Maera_Caching::cache_duration()
-		);
+		Maera_Template::main( 'site-logo.twig' );
 	}
 
 
