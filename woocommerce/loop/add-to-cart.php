6bf124df47319461f63d0f1f592a414bc0563dda
Aristeides Stathopoulos <aristath@gmail.com>
2015-05-14 22:07:37 +0300

    template for loop buttons

---
+++ woocommerce/loop/add-to-cart.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Loop Add to Cart
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $product;
+
+$context = array(
+	'product'  => $product,
+	'quantity' => $quantity,
+);
+Maera()->views->render( 'loop/add-to-cart.twig', $context );
