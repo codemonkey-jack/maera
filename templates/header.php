92f971cea45bca8c4e700b22b282d6874b2d05ab
Aristeides Stathopoulos <aristath@gmail.com>
2015-07-25 03:25:17 +0300

    move <head> to a separate template-part

--- templates/header.php
+++ templates/header.php
@@ -9,15 +9,7 @@
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<?php wp_head(); ?>
-</head>
-
+<?php maera_get_template_part( 'template-parts/head' ); ?>
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'maera' ); ?></a>
