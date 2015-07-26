01354afc7788b171ea5c3c286f867174b9579041
Aristeides Stathopoulos <aristath@gmail.com>
2015-07-26 03:03:12 +0300

    new header

--- mdl-shell/class-maera-mdl-menu-navwalker.php
+++ mdl-shell/class-maera-mdl-menu-navwalker.php
@@ -7,7 +7,7 @@ class Maera_MDL_Menu_Navwalker extends Walker_Nav_Menu {
 
         foreach ( $elements as $item ) {
             if ( $item->current ) {
-                $list .= '<a class="mdl-navigation__link active" href=' . $item->url . '>' . $item->title . '</a>';
+                $list .= '<a class="mdl-navigation__link is-active" href=' . $item->url . '>' . $item->title . '</a>';
             } else {
                 $list .= '<a class="mdl-navigation__link" href=' . $item->url . '>' . $item->title . '</a>';
             }
--- mdl-shell/templates/template-parts/header.php
+++ mdl-shell/templates/template-parts/header.php
@@ -1,34 +1,29 @@
-<div class="layout-transparent mdl-layout mdl-js-layout">
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
     <header class="mdl-layout__header">
         <div class="mdl-layout__header-row">
-            <!-- Title -->
-            <span class="mdl-layout-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-            <!-- Add spacer, to align navigation to the right -->
             <div class="mdl-layout-spacer"></div>
-            <!-- Navigation -->
-            <?php
-            wp_nav_menu( array(
-                'theme_location' => 'primary',
-                'walker'         => new Maera_MDL_Menu_Navwalker,
-                'container'      => 'nav',
-                'container_class' => 'mdl-navigation mdl-layout--large-screen-only',
-                'items_wrap'     => '%3$s',
-                'depth'          => -1,
-            ) );
-            ?>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+                <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
+                    <i class="material-icons">search</i>
+                </label>
+                <div class="mdl-textfield__expandable-holder">
+                    <input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp" />
+                </div>
+            </div>
         </div>
     </header>
     <div class="mdl-layout__drawer">
-        <span class="mdl-layout-title">Title</span>
+        <span class="mdl-layout-title">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+        </span>
         <?php
         wp_nav_menu( array(
             'theme_location' => 'primary',
             'walker'         => new Maera_MDL_Menu_Navwalker,
             'container'      => 'nav',
             'container_class' => 'mdl-navigation',
-            'items_wrap'     => '',
+            'items_wrap'     => '%3$s',
             'depth'          => -1,
         ) );
         ?>
     </div>
-    <div class="mdl-layout__drawer-button"><i class="material-icons">menu</i></div>
