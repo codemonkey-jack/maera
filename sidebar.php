e599d42ec514b43b0ac90922fec4b1e109eea714
Aristeides Stathopoulos <aristath@gmail.com>
2015-07-29 06:20:38 +0300

    Use default templates from _s as fallbacks

--- sidebar.php
+++ sidebar.php
@@ -1,2 +1,15 @@
 <?php
-Maera::get_template_part( 'sidebar' );
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package Maera
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<div id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div><!-- #secondary -->
