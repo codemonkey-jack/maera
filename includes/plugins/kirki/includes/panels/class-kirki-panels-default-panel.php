aad32da1cf9be33fe2a7a40097addab8d071e335
Jack Kitterhing <codingmonkeyjack@gmail.com>
2016-08-23 19:25:13 +0100

    Remove Kirki and Timber as embedded Plugins.

--- includes/plugins/kirki/includes/panels/class-kirki-panels-default-panel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Kirki_Panels_Default_Panel extends WP_Customize_Panel {
-
-	public $type = 'kirki-default';
-
-}
--- includes/plugins/kirki/includes/settings/class-kirki-settings-default-setting.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ( ! class_exists( 'Kirki_Settings_Default_Setting' ) ) {
-
-	class Kirki_Settings_Default_Setting extends WP_Customize_Setting {}
-
-}
